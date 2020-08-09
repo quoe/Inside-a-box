908f9e379a1e17aaae9b5486c79c74943cc6d22d
Кучеров Роман <quoe@mail.ru>
2020-08-09 11:46:33 +0300

    Начало

---
+++ Assets/ColliderFromSphere.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ColliderFromSphere : MonoBehaviour {
+
+    private  Vector3[] raycastDirections;
+    public GameObject Target;
+    public int MaxIterations = 10000;
+    Collider TargetСollider;
+    Vector3 colliderBoundsMin;
+    Vector3 colliderBoundsMax;
+    Vector3 Pos;
+    private bool isInside;
+    private bool isSaved = false;
+
+    // Use this for initialization
+    void Start () {
+        NewPos();
+    }
+
+    public bool InCollider(GameObject TargetObject) {
+        Collider TargetColl = TargetObject.GetComponent<Collider>();
+        Vector3 TargetPos_ = TargetObject.transform.position;
+        Vector3 SelfPos = transform.position;
+        Ray ray = new Ray(SelfPos, (TargetPos_ - SelfPos));
+        RaycastHit Hit = new RaycastHit();
+        if (TargetСollider.Raycast(ray, out Hit, 100))
+        {
+            Debug.DrawLine(SelfPos, Hit.point, Color.red);
+            return false;
+        }
+        else { return true; }
+    }
+
+    private void NewPos()
+    {
+        TargetСollider = Target.GetComponent<Collider>();
+        colliderBoundsMin = TargetСollider.bounds.min;
+        colliderBoundsMax = TargetСollider.bounds.max;
+        Pos.x = Random.Range(colliderBoundsMin.x, colliderBoundsMax.x);
+        Pos.y = Random.Range(colliderBoundsMin.y, colliderBoundsMax.y);
+        Pos.z = Random.Range(colliderBoundsMin.z, colliderBoundsMax.z);
+        transform.position = Pos;
+    }
+
+    public bool IsBoundsContained(Vector3 targetPoint, Collider collider)
+    {
+        if (collider.bounds.Contains(targetPoint)) { return true; }
+        return false;
+    }
+
+    public void File_AppendText(string FilePath, string AppendString)
+    {
+        using (StreamWriter sw = File.AppendText(FilePath))
+        {
+            sw.WriteLine(AppendString);
+        }
+    }
+
+    void Update()
+    {
+        isInside = InCollider(Target);
+        if (!isInside) NewPos();
+        if (isInside && !isSaved)
+        {
+            isSaved = true;
+            float x = transform.position.x;
+            float y = transform.position.y;
+            float z = transform.position.z;
+            float x_rot = Target.transform.rotation.x;
+            float y_rot = Target.transform.rotation.y;
+            float z_rot = Target.transform.rotation.z;
+            string ParamStr = Target.name + "\t" + x.ToString() + "\t" + y.ToString() + "\t" + z.ToString() + "\t" + x_rot.ToString() + "\t" + y_rot.ToString() + "\t" + z_rot.ToString();
+            File_AppendText("Result_Name_xyzSelfPos_xyzTargetRotation.txt", ParamStr);
+        }
+    }
+}
+
+
---
+++ Assets/ColliderUtils.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderUtils : MonoBehaviour {
+
+    private  Vector3[] raycastDirections;
+    private Vector3 raycastDirections1;
+    public GameObject Target;
+    public float maxDistance = 100;
+
+    // Use this for initialization
+    void Start () {
+        raycastDirections = new Vector3[5];
+        raycastDirections[0] = new Vector3(0, 1, 0);
+        raycastDirections[1] = new Vector3(0, -1, -0);
+        raycastDirections[2] = new Vector3(0, 0, 1);
+        raycastDirections[3] = new Vector3(-1.41f, 0, -0.5f);
+        raycastDirections[4] = new Vector3(1.41f, 0, -0.5f);
+
+        raycastDirections1 = new Vector3(0, 1, 0);
+    }
+
+    public bool IsContained(Vector3 targetPoint, Collider collider)
+    {
+        // A quick check - if the bounds doesn't contain targetPoint, then it definitely can't be contained in the collider
+        if (!collider.bounds.Contains(targetPoint))
+        {
+            Debug.Log("Вне формы");
+            return false;
+        }
+
+        // The "100f * direction" is a magic number so that we 
+        // start far enough from the point.
+        foreach (Vector3 direction in raycastDirections)
+        {
+            Debug.DrawLine(targetPoint, targetPoint + direction, Color.red);
+        }
+
+        foreach (Vector3 direction in raycastDirections)
+        {
+            Ray ray = new Ray(targetPoint, targetPoint + direction);
+            RaycastHit dummyHit = new RaycastHit();
+            // dummyHit because collider.Raycast requires a RaycastHit
+            if (!collider.Raycast(ray, out dummyHit, maxDistance))
+            {
+                Debug.Log("Один луч не касается");
+                return false;
+            }
+        }
+        Debug.Log("В форме");
+        return true;
+    }
+
+    public bool IsContained2(Vector3 targetPoint, Collider collider)
+    {
+        // A quick check - if the bounds doesn't contain targetPoint, then it definitely can't be contained in the collider
+        if (!collider.bounds.Contains(targetPoint))
+        {
+            Debug.Log("Вне формы");
+            //return false;
+        }
+
+        // The "100f * direction" is a magic number so that we 
+        // start far enough from the point.
+        //Debug.DrawLine(targetPoint, targetPoint + raycastDirections1, Color.red);
+
+        Ray ray = new Ray(targetPoint, raycastDirections1);
+        RaycastHit dummyHit = new RaycastHit();
+        // dummyHit because collider.Raycast requires a RaycastHit
+        if (!collider.Raycast(ray, out dummyHit, maxDistance))
+        {
+
+            //Debug.Log("(!collider.Raycast(ray, out dummyHit, maxDistance))" + dummyHit.collider.name + " distance=" + dummyHit.distance);
+            //return false;
+        }
+        if (collider.Raycast(ray, out dummyHit, maxDistance))
+        {
+            Debug.Log("collider.Raycast(ray, out dummyHit, maxDistance)" + dummyHit.collider.name + " distance=" + dummyHit.distance);
+            Debug.DrawLine(targetPoint, dummyHit.point, Color.red);
+            //return false;
+        }
+        //Debug.Log("Конец");
+        return true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawLine(contact.point, contact.point + contact.normal, Color.black);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider Coll = GetComponent<Collider>();
+        Vector3 targetPoint = Target.transform.position;
+        IsContained2(targetPoint, Coll);
+    }
+
+
+}
